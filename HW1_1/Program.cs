043602e3b321e6c04a0c829457dcd8cfce0c8e1b
flistrr <147997354+flistrr@users.noreply.github.com>
2023-11-08 19:47:21 +0200

    Add files via upload

---
+++ HW1_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        public class Evaluation
+        {
+            public static void Main(string[] args)
+            {
+                var leg = Console.ReadLine();
+                int a;
+                bool success = Int32.TryParse(leg, out a);
+                if (success)
+                {
+                    double area = a * a;
+                    double perimiter = a * 4;
+                    Console.WriteLine("Area: {0}", area);
+                    Console.WriteLine("Perimeter: {0}", perimiter);
+                }
+                else
+                {
+                    Console.WriteLine("Please, enter an integer value");
+                }
+            }
+        }
+
+
+    }
+}
