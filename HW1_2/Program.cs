7c88f071e9e9e80355feddc05d203fd172f3f6ad
flistrr <147997354+flistrr@users.noreply.github.com>
2023-11-08 19:48:04 +0200

    Add files via upload

---
+++ HW1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HW1_2
+{
+    public class Evaluation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("How old are you, {0}?", name);
+            int age = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Hello {name}, you are {age} years old :)");
+        }
+    }
+}
